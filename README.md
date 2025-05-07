559b808053264266e7d7aa20d86be68b8e643eb4
mbakkari <mbakkari@norsys.fr>
2025-05-07 10:22:54 +0200

    Add README and refactor exception handling placement

---
+++ README.md
@@ -0,0 +1,58 @@
+# 🏥 Diagnostic health routing API 
+
+## 📌 Description
+
+Cette api permet à partir d’un index de santé de déterminer l’unité médicale dans laquelle le patient doit être orienté.
+
+
+---
+
+## 🚀 Cloner et démarrer le projet
+
+### 🔧 Prérequis
+
+- Java 17
+- Maven
+
+### ▶️ Lancer l'application
+
+```
+git clone https://github.com/bakkari1u/DiagonsticHealthRouting.git
+cd diagnostic
+mvn clean install
+mvn spring-boot:run
+```
+
+### 🧪 Lancer les tests (unitaires et d'intégrations)
+
+```
+mvn test
+```
+
+### 📖 Documentation Swagger / OpenAPI
+
+```
+Une fois l'application démarrée, accédez à :
+
+👉 http://localhost:8080/swagger-ui.html
+```
+
+### 🔍 Utilisation de l’API
+
+```
+🧠 Endpoint principal : 
+
+👉 GET /api/health-routing/{index}
+
+✅ Exemples des requetes et réponses attendues : 
+
+| Requête                  | Réponse attendue           |
+| ------------------------ | -------------------------- |
+| `/api/health-routing/33` | Cardiologie                |
+| `/api/health-routing/55` | Traumatologie              |
+| `/api/health-routing/15` | Cardiologie, Traumatologie |
+| `/api/health-routing/7`  | Aucune pathologie détectée |
+
+
+```
+
--- src/main/java/com/hopital/diagnostic/controller/HealthRoutingController.java
+++ src/main/java/com/hopital/diagnostic/controller/HealthRoutingController.java
@@ -5,10 +5,12 @@ import com.hopital.diagnostic.service.MedicalUnitsResolverService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestController
 @RequestMapping("/api/health-routing")
@@ -23,5 +25,12 @@ public class HealthRoutingController {
     public ResponseEntity<String> getUnits(@PathVariable int index) {
         String unit =  medicalUnitsResolverService.resolveUnits(index);
         return ResponseEntity.ok(unit);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body("L'index doit être un entier valide (ex : 15, 33).");
     }
 }
